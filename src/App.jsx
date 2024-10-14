5556c368baac5f1c6da4a5e22b676c22d8e15243
LUISANGELMERIDA <115110361+LUISANGELMERIDA@users.noreply.github.com>
2024-10-14 17:08:42 -0600

    se agrego archivo index

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, {useStatate} from "react";
+
+function App() {
+    const [name, setName] = useState("");
+    return (
+        <div className="app">
+        <h1>
+            Hola React
+        </h1>
+        <div>
+            <label htmlFor="name">Nombre: </label>
+            <input
+                id="name"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+        </div>
+        </div>
+    );
+}
+
+export default App;
