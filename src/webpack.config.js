37ddea16e0589e8835f137815d92e8139285d59f
LUISANGELMERIDA <115110361+LUISANGELMERIDA@users.noreply.github.com>
2024-10-20 17:39:19 -0600

    se configuro las  extensiones en webpack  js jsx y json

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -4,7 +4,7 @@ module.exports ={
     entry:"./src/index.js",
     output: {
         path:path.resolve(__dirname, "dist"),
-        name: "[name].[contenthash].js",
+        filename: "[name].[contenthash].js",
         publicPath: ""
     },
     mode: "production",
@@ -14,8 +14,11 @@ module.exports ={
             use:"babel-loader",
             test:/.(js jsx)$/,
             exclude:/node_modules/
-          }
-        ]
+          },
+        ],
+    },
+    resolve: {
+        extensions: [".js","jsx","json"]
     }
 
-}
+};
